d3758f8a39f98a757050697455b15bf44294c037
Leonardo Garcia <leo.equih@gmail.com>
2020-02-20 16:35:36 -0600

    Creating BNREmployee class extension

--- BMITime/BMITime/BNREmployee.h
+++ BMITime/BMITime/BNREmployee.h
@@ -14,12 +14,8 @@ NS_ASSUME_NONNULL_BEGIN
 @class BNRAsset;
 
 @interface BNREmployee : BMIPerson
-{
-    NSMutableArray *_assets;
-}
 
 @property (nonatomic) unsigned int employeeID;
-@property (nonatomic) unsigned int officeAlarmCode;
 @property (nonatomic) NSDate *hireDate;
 @property (nonatomic, copy) NSArray *assets;
 
